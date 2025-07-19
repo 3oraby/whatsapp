f88a7617adac6daa94b3aaebf9fb9b9850644012
3oraby <mohaahmed21102003@gmail.com>
2025-07-19 20:18:32 +0300

    add new base url

---
+++ lib/core/api/api_urls.dart
@@ -0,0 +1,6 @@
+class ApiUrls {
+  static const String baseUrl = "http://10.0.2.2/";
+
+  static const String userApiBaseUrl = "user/api";
+  static const String chatApiBaseUrl = "chat/api";
+}
--- lib/core/api/end_points.dart
+++ lib/core/api/end_points.dart
@@ -1,54 +1,54 @@
+import 'package:whatsapp/core/api/api_urls.dart';
+
 class EndPoints {
-  static const String baseUrl = "http://10.0.2.2:5000/api/";
-  static const String webSocketUrl = "http://10.0.2.2:3000/api/";
+  // Auth
+  static const String login = "${ApiUrls.userApiBaseUrl}/auth/login";
+  static const String signup = "${ApiUrls.userApiBaseUrl}/auth/signup";
+  static const String verifyOTP = "${ApiUrls.userApiBaseUrl}/auth/verifyOTP";
+  static const String resendOTP = "${ApiUrls.userApiBaseUrl}/auth/resendOTP";
+  static const String refreshToken = "${ApiUrls.userApiBaseUrl}/auth/refreshToken";
 
-  // auth
-  static const String login = "${baseUrl}auth/login";
-  static const String signup = "${baseUrl}auth/signup";
-  static const String verifyOTP = "${baseUrl}auth/verifyOTP";
-  static const String resendOTP = "${baseUrl}auth/resendOTP";
-  static const String refreshToken = "${baseUrl}auth/refreshToken";
-
-  // user
-  static const String searchInUsers = "${baseUrl}user/search";
-  static const String getUser = "${baseUrl}user";
-  static const String updateUser = "${baseUrl}user";
-  static const String uploadProfileImage = "${baseUrl}user/image";
+  // User
+  static const String getUser = "${ApiUrls.userApiBaseUrl}/user";
+  static const String updateUser = "${ApiUrls.userApiBaseUrl}/user";
+  static const String uploadProfileImage = "${ApiUrls.userApiBaseUrl}/user/image";
+  static const String saveFcmToken = "${ApiUrls.userApiBaseUrl}/register-fcm-token";
+  static const String searchInUsers = "${ApiUrls.userApiBaseUrl}/user/search";
   static String getUserCurrentStatus({required int userId}) =>
-      "${baseUrl}user/currentStatus/$userId";
+      "${ApiUrls.userApiBaseUrl}/user/currentStatus/$userId";
 
-  // contacts
-  static const String getUserContacts = "${baseUrl}contact";
-  static const String createContact = "${baseUrl}contact";
+  // Contacts
+  static const String getUserContacts = "${ApiUrls.userApiBaseUrl}/contact";
+  static const String createContact = "${ApiUrls.userApiBaseUrl}/contact";
 
-  // stories
-  static const String createStory = "${baseUrl}status";
-  static const String getCurrentUserStory = "${baseUrl}status";
+  // Stories
+  static const String createStory = "${ApiUrls.userApiBaseUrl}/status";
+  static const String getCurrentUserStory = "${ApiUrls.userApiBaseUrl}/status";
   static const String getUserContactsStory =
-      "${baseUrl}status/getUserCotactsStatus";
+      "${ApiUrls.userApiBaseUrl}/status/getUserCotactsStatus";
   static String viewStory({required int storyId}) =>
-      "${baseUrl}status/view/$storyId";
+      "${ApiUrls.userApiBaseUrl}/status/view/$storyId";
   static String deleteStory({required int storyId}) =>
-      "${baseUrl}status/$storyId";
+      "${ApiUrls.userApiBaseUrl}/status/$storyId";
   static String reactStory({required int storyId}) =>
-      "${baseUrl}status/react/$storyId";
+      "${ApiUrls.userApiBaseUrl}/status/react/$storyId";
   static String getReactsOnStory({required String storyId}) =>
-      "${baseUrl}status/reacts/$storyId";
+      "${ApiUrls.userApiBaseUrl}/status/reacts/$storyId";
   static String getViewsOnStory({required String storyId}) =>
-      "${baseUrl}status/views/$storyId";
+      "${ApiUrls.userApiBaseUrl}/status/views/$storyId";
 
-  // chats (uses webSocketUrl instead of baseUrl)
-  static const String createChat = "${webSocketUrl}chat";
-  static const String getUserChats = "${webSocketUrl}chat";
-  static const String uploadChatImage = "${webSocketUrl}chat/uploadChatImage";
+  // Chats
+  static const String createChat = "${ApiUrls.chatApiBaseUrl}/chat";
+  static const String getUserChats = "${ApiUrls.chatApiBaseUrl}/chat";
+  static const String uploadChatImage = "${ApiUrls.chatApiBaseUrl}/chat/uploadChatImage";
   static String pinChat({required int chatId}) =>
-      "${webSocketUrl}chat/pin/$chatId";
+      "${ApiUrls.chatApiBaseUrl}/chat/pin/$chatId";
   static String unPinChat({required int chatId}) =>
-      "${webSocketUrl}chat/unPin/$chatId";
+      "${ApiUrls.chatApiBaseUrl}/chat/unPin/$chatId";
   static String getChatMessages({required int chatId}) =>
-      "${webSocketUrl}chat/messages/$chatId";
+      "${ApiUrls.chatApiBaseUrl}/chat/messages/$chatId";
   static String getMessageHistory({required int messageId}) =>
-      "${webSocketUrl}chat/message/$messageId/history";
+      "${ApiUrls.chatApiBaseUrl}/chat/message/$messageId/history";
   static String getMessageReacts({required int messageId}) =>
-      "${webSocketUrl}chat/message/$messageId/reacts";
+      "${ApiUrls.chatApiBaseUrl}/chat/message/$messageId/reacts";
 }
